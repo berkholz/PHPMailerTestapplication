c669c7af84d3fe570457d3322cfbc2bb6fe751ed
root <root@icinga3>
2016-12-22 20:37:36 +0100

    change variable name to a more speaking description

--- mail-config.php
+++ mail-config.php
@@ -5,6 +5,8 @@ $TO_TEMPLATE = "root@localhost";
 $FROM_TEMPLATE = "root@localhost";
 $SUBJECT_TEMPLATE = "Testnachricht von " . $_SERVER['SERVER_NAME'];
 $MESSAGE_TEMPLATE = "Dies ist eine Testnachricht um " . date(DATE_RFC822);
-$SSL_VERIFY_TEMPLATE = "";
+
+// set to true to accept self signed certificates, otherwise false
+$SSL_NOVERIFY_CHECKED = "true";
 
 ?>
